4d3d32f74936f6488b2638bdd6515faf0c0246e8
Kinukles <mdavis110@live.spcollege.edu>
2026-09-12 23:06:09 -0400

    Add Ch07ExGuitarShop exercise

---
+++ Ch07ExGuitarShop/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GuitarShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("[action]")]
+        public IActionResult About()
+        {
+            return View();
+        }
+        [Route("ContactUs")]
+        public IActionResult ContactUs()
+        {
+            var contact = new Dictionary<string, string>
+            {
+                ["Email"] = "info@myguitarshop.com",
+                ["Phone"] = "(555) 123-4567",
+            };
+            return View(contact);
+        }
+    }
+}
